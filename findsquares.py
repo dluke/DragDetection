aafc10d323ee5d9bd8d54f94c14c507422ea56d7
C S <cswart@outlook.com>
2015-08-16 15:39:27 +0100

    draws the contour of the big square

--- findsquares.py
+++ findsquares.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import cv2
 
@@ -9,30 +7,6 @@ image = sys.argv[1]
 
 # Load an color image in grayscale
 img = cv2.imread(image)
-
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-lower_red = np.array([0, 60, 50])
-upper_red = np.array([180, 255, 200])
-
-mask = cv2.inRange(hsv, lower_red, upper_red)
-
-res = cv2.bitwise_and(img,img, mask= mask)
-
-cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
-cv2.namedWindow('res', cv2.WINDOW_NORMAL)
-
-#cv2.imshow('img',img)
-#cv2.imshow('mask',mask)
-#cv2.imshow('res',res)
-
-
-#cv2.waitKey(0)
-
-##cv2.imshow('image',img)
-#cv2.destroyAllWindows()
-
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -45,19 +19,20 @@ ret,thresh = cv2.threshold(gray,127,255,0)
 
 contours,h = cv2.findContours(thresh,1,2)
 
+squares = []
 count = 0
 for cnt in contours:
-    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    print len(approx)
+    approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+    area = cv2.contourArea(cnt)
 
-    if len(approx)==4:
-        count += 1
-        print "square"
-        cv2.drawContours(img,[cnt],0,(128,255,0),4)
-        #cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-        #cv2.imshow('img',img)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+    if len(approx)==4 and (area > 10000):
+        squares.append({'area': area, 'contour':cnt})
+        count = count + 1
+
+sorted_squares = sorted(squares, key=lambda k: k['area'])
+big_square_contour = sorted_squares[-2]['contour']
+cv2.drawContours(img,[big_square_contour],0,(128,255,0),4)
+
 
 print 'count', count
 
